873386fb81d7adf7111fc9fb379205a65f8283a6
John Wang <johncwang@gmail.com>
2023-09-03 10:53:09 -0700

    feat: `rdsutil`: add `RDSClientMore{}`, `NewRDSClientMore()`, `NewRDSClient()`

---
+++ rdsutil/client.go
@@ -0,0 +1,39 @@
+﻿package rdsutil
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/grokify/awsgo/config"
+)
+
+type RDSClientMore struct {
+	client *rds.RDS
+}
+
+func NewRDSClientMore(cm *config.AWSConfigMore) (*RDSClientMore, error) {
+	if svc, err := NewRDSClient(cm); err != nil {
+		return nil, err
+	} else {
+		return &RDSClientMore{client: svc}, nil
+	}
+}
+
+func NewRDSClient(cm *config.AWSConfigMore) (*rds.RDS, error) {
+	if cm == nil {
+		return nil, errors.New("config.AWSConfigMore cannot be nil")
+	}
+	mySession, err := cm.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	cfgs := []*aws.Config{}
+	region := strings.TrimSpace(cm.Region)
+	if region != "" {
+		cfgs = append(cfgs, aws.NewConfig().WithRegion(region))
+	}
+	svc := rds.New(mySession, cfgs...)
+	return svc, nil
+}
