d78742ee73f5d3f5ba42d5372a0e436d10771ee4
John Wang <johncwang@gmail.com>
2025-04-07 05:09:57 -0700

    feat: `inspector2util`: add `Packages{}`

--- inspector2util/package.go
+++ inspector2util/package.go
@@ -1,7 +1,110 @@
 package inspector2util
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/aws/aws-sdk-go-v2/service/inspector2/types"
 )
 
 type Package types.VulnerablePackage
+
+func (p Package) NameAndFilepathAtVersion() string {
+	name := p.NameString()
+	fp := p.FilepathString()
+	if name != "" && fp != "" {
+		return fmt.Sprintf("%s (%s)", p.NameAtVersion(), p.FilepathAtVersion())
+	} else if name != "" {
+		return p.NameAtVersion()
+	} else if fp != "" {
+		return p.FilepathAtVersion()
+	} else {
+		return ""
+	}
+}
+
+func (p Package) NameAndFilepathAtVersionFixed() string {
+	name := p.NameString()
+	fp := p.FilepathString()
+	if name != "" && fp != "" {
+		return fmt.Sprintf("%s (%s)", p.NameAtVersionFixed(), p.FilepathAtVersionFixed())
+	} else if name != "" {
+		return p.NameAtVersionFixed()
+	} else if fp != "" {
+		return p.FilepathAtVersionFixed()
+	} else {
+		return ""
+	}
+}
+
+func (p Package) FilepathAtVersion() string {
+	fp := p.FilepathString()
+	ver := p.VersionString()
+	if fp == "" && ver == "" {
+		return ""
+	} else {
+		return fp + sepFilepathVersion + ver
+	}
+}
+
+func (p Package) FilepathAtVersionFixed() string {
+	fp := p.FilepathString()
+	ver := p.VersionFixedString()
+	if fp == "" && ver == "" {
+		return ""
+	} else {
+		return fp + sepFilepathVersion + ver
+	}
+}
+
+func (p Package) FilepathString() string {
+	if p.FilePath == nil {
+		return ""
+	} else {
+		return strings.TrimSpace(*p.FilePath)
+	}
+}
+
+func (p Package) NameAtVersion() string {
+	name := p.NameString()
+	ver := p.VersionString()
+	if name == "" && ver == "" {
+		return ""
+	} else {
+		return name + sepFilepathVersion + ver
+	}
+}
+
+func (p Package) NameAtVersionFixed() string {
+	fp := p.NameString()
+	ver := p.VersionFixedString()
+	if fp == "" && ver == "" {
+		return ""
+	} else {
+		return fp + sepFilepathVersion + ver
+	}
+}
+
+func (p Package) NameString() string {
+	if p.Name == nil {
+		return ""
+	} else {
+		return strings.TrimSpace(*p.Name)
+	}
+}
+
+func (p Package) VersionString() string {
+	if p.Version == nil {
+		return ""
+	} else {
+		return strings.TrimSpace(*p.Version)
+	}
+}
+
+func (p Package) VersionFixedString() string {
+	if p.FixedInVersion == nil {
+		return ""
+	} else {
+		return strings.TrimSpace(*p.FixedInVersion)
+	}
+}
---
+++ inspector2util/packages.go
@@ -0,0 +1,58 @@
+package inspector2util
+
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/inspector2/types"
+	"github.com/grokify/mogo/type/stringsutil"
+)
+
+type Packages []types.VulnerablePackage
+
+func (ps Packages) FilepathsAtVersion() string {
+	var paths []string
+	for _, p := range ps {
+		if v := Package(p).FilepathAtVersion(); v != "" {
+			paths = append(paths, v)
+		}
+	}
+	return sliceCondenseSpaceAndJoin(paths)
+}
+
+func (ps Packages) FilepathsAtVersionFixed() string {
+	var paths []string
+	for _, p := range ps {
+		if v := Package(p).FilepathAtVersionFixed(); v != "" {
+			paths = append(paths, v)
+		}
+	}
+	return sliceCondenseSpaceAndJoin(paths)
+}
+
+func (ps Packages) NamesAndFilepathsAtVersion() string {
+	var paths []string
+	for _, p := range ps {
+		if v := Package(p).NameAndFilepathAtVersion(); v != "" {
+			paths = append(paths, v)
+		}
+	}
+	return sliceCondenseSpaceAndJoin(paths)
+}
+
+func (ps Packages) NamesAndFilepathsAtVersionFixed() string {
+	var paths []string
+	for _, p := range ps {
+		if v := Package(p).NameAndFilepathAtVersionFixed(); v != "" {
+			paths = append(paths, v)
+		}
+	}
+	return sliceCondenseSpaceAndJoin(paths)
+}
+
+func sliceCondenseSpaceAndJoin(s []string) string {
+	if s = stringsutil.SliceCondenseSpace(s, true, true); len(s) == 0 {
+		return ""
+	} else {
+		return strings.Join(s, sepJoinDispolay)
+	}
+}
